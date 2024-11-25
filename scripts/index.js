faa507e2d04dc1ed3f17cee5fc525388413f3182
Konstantin Shkityr <shkityr.k@yandex.ru>
2024-11-25 11:00:58 +0300

    feat: add render cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,17 @@
-// @todo: Темплейт карточки
+function createCard(name, link){
+    const cardTemplate = document.querySelector("#card-template").content
+    const cardElement = cardTemplate.querySelector(".card").cloneNode(true)
+    cardElement.querySelector('.card__title').textContent = name
+    cardElement.querySelector('.card__image').src = link
+    return cardElement
+}
+function renderCards(){
+    const placesList = document.querySelector(".places__list")
+    initialCards.forEach(element => {
+        placesList.append(createCard(element.name, element.link))
+    })
+}
+renderCards();
 
 // @todo: DOM узлы
 
