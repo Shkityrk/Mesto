600b660dc6ec9fe173519f51de1de66be71cebd7
shkityrk <shkityr.k@yandex.ru>
2024-12-14 00:41:46 +0300

    feat:add validation and closing forms by 'esc' and by clicking overlay

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,3 +1,7 @@
+import { openImageModal } from ".";
+
+const cardTemplate = document.querySelector("#card-template").content.querySelector(".card");
+
 export const initialCards = [
     {
       name: "Архыз",
@@ -24,3 +28,32 @@ export const initialCards = [
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
     }
 ];
+
+export const createCard = (name, link, cardSettings) => {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(cardSettings.cardImage);
+  cardElement.querySelector(cardSettings.cardTitle).textContent = name;
+  cardImage.src = link;
+  cardImage.alt = name;
+
+  cardElement.querySelector(cardSettings.cardLikeButton).addEventListener("click", (e) => {
+    e.target.classList.toggle(cardSettings.cardLikeButtonActive);
+  });
+
+  cardElement.querySelector(cardSettings.cardDeleteButton).addEventListener("click", (e) => {
+    e.target.closest(cardSettings.card).remove();
+  });
+
+	cardElement.querySelector(cardSettings.cardImage).addEventListener("click", (e) => {
+		console.log(cardElement)
+		openImageModal(cardElement.querySelector(cardSettings.cardImage).src, cardElement.querySelector(cardSettings.cardTitle).textContent)
+	})
+
+  return cardElement;
+}
+
+export const renderCards = (placesList, cardSettings) => {
+  initialCards.forEach(element => {
+    placesList.append(createCard(element.name, element.link, cardSettings));
+  });
+}
